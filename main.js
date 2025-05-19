f49027ee0fbddea4b256b90c74ab8ec481b593d7
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2025-05-18 23:07:05 -0500

    Updated index.html

--- main.js
+++ main.js
@@ -708,4 +708,34 @@ filterPlugins();
     }
   }
   loadReviews();
+
+  let times = 0;
+  function loadCounter() {
+   let link = atob("aHR0cHM6Ly9hbG9uc29hbGlhZ2EtcGFnZS1jb3VudC5nbGl0Y2gubWUvY291bnRlcj9zaXRlPTxzaXRlPiZrZXk9PGtleT4=").replace(/<site>/g,"alonsoaliaga").replace(/<key>/g,"KEY-A");
+   let counter = document.getElementById("visitor-counter");
+   if(counter) {
+     $.ajax({
+       url: link,
+       type: "GET", /* or type:"GET" or type:"PUT" */
+       dataType: "json",
+       data: {
+       },
+       success: function (result) {
+         if(isNaN(result))
+           document.getElementById("counter-amount").innerHTML = "Click to return!";
+         else document.getElementById("counter-amount").innerHTML = `Visits: ${result}`;
+       },
+       error: function (e) {
+         times++;
+         document.getElementById("counter-amount").innerHTML = "Click to return!";
+         if(times <= 1) {
+          setTimeout(()=>{
+            loadCounter();
+          },1000*10);
+         }
+       }
+     });
+   }
+  }
+  loadCounter();
 })
